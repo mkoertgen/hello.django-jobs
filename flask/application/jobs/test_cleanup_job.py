652ea2cba6b81e9b9a695e5ecd7f68b79fba1667
mkoertgen <marcel.koertgen@gmail.com>
2019-11-20 00:48:43 +0100

    Refactoring

--- flask/application/jobs/test_cleanup_job.py
+++
@@ -1,9 +0,0 @@
-import logging
-from unittest.mock import MagicMock
-from jobs.util.test_base import IntegrationTestCase
-from jobs.scheduler.cleanup_job import CleanupJob
-
-
-class TestCleanupJob(IntegrationTestCase):
-    def test_job(self):
-        CleanupJob().perform()
